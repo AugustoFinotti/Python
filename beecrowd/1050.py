353c54a0349f21fccdaec343ec62b86a4e753068
Augusto Finotti Oliveira <augustofoliver@gmail.com>
2022-04-06 17:57:23 -0300

    adding 1050.py

---
+++ beecrowd/1050.py
@@ -0,0 +1,28 @@
+ddd = int(input())
+
+if(ddd == 61):
+    city = "Brasilia"
+    print(city)
+elif(ddd == 71):
+    city = "Salvador"
+    print(city)
+elif(ddd == 11):
+    city = "Sao Paulo"
+    print(city)
+elif(ddd == 21):
+    city = "Rio de Janeiro"
+    print(city)
+elif(ddd == 32):
+    city = "Juiz de Fora"
+    print(city)
+elif(ddd == 19):
+    city = "Campinas"
+    print(city)
+elif(ddd == 27):
+    city = "Vitoria"
+    print(city)
+elif(ddd == 31):
+    city = "Belo Horizonte"
+    print(city)
+else:
+    print("DDD nao cadastrado")
