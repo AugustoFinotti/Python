b8946d5357b3ab2c1589832074c4a0e1df1e20ef
Augusto Finotti Oliveira <augustofoliver@gmail.com>
2022-04-14 08:35:33 -0300

    adding 1060.py

---
+++ beecrowd/1060.py
@@ -0,0 +1,21 @@
+v1 = float(input())
+v2 = float(input())
+v3 = float(input())
+v4 = float(input())
+v5 = float(input())
+v6 = float(input())
+num_pos = 0
+if(v1>0):
+    num_pos = num_pos +1
+if(v2>0):
+    num_pos = num_pos +1
+if(v3>0):
+    num_pos = num_pos +1
+if(v4>0):
+    num_pos = num_pos +1
+if(v5>0):
+    num_pos = num_pos +1
+if(v6>0):
+    num_pos = num_pos +1
+num_pos = int(num_pos)
+print("%d valores positivos" %(num_pos))
