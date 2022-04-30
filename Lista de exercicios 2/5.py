37d75cfffc6bbe5abd96aec2d28c7e51275eea5a
Augusto Finotti Oliveira <augustofoliver@gmail.com>
2022-04-30 17:20:43 -0300

    adding 5.py

---
+++ Lista de exercicios 2/5.py
@@ -0,0 +1,15 @@
+# FATEC Ourinhos - Programação 1 - Seg Info
+# Augusto Finotti Oliveira
+# Prof. Rogério Lazanha
+# 30/04/2022
+
+# 5- Faca um programa que receba um numero inteiro
+# e verifique se este numero e par ou ımpar.
+
+# Input
+num = int(input("Digite um número: "))
+# Processing and Output
+if(num%2 == 0):
+    print("O número é par!")
+else:
+    print("O número é ímpar!")
