d51710e24b514ddb869ed67d5d5dacfcd2e28497
Augusto Finotti Oliveira <augustofoliver@gmail.com>
2022-05-01 16:58:19 -0300

    adding 7.py

---
+++ Lista de exercicios 3/7.py
@@ -0,0 +1,7 @@
+# FATEC Ourinhos - Programação 1 - Seg Info
+# Augusto Finotti Oliveira
+# Prof. Rogério Lazanha
+# 01/05/2022
+
+# 7- Faca um programa que leia 10 inteiros positivos, ignorando nao positivos, e imprima sua media. 
+
