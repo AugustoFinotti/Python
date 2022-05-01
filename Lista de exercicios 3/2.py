7671fd862c045d844903957d99d91c16d67abacd
Augusto Finotti Oliveira <augustofoliver@gmail.com>
2022-05-01 16:57:39 -0300

    adding 2.py

--- Lista de exercicios 3/2.py
+++ Lista de exercicios 3/2.py
@@ -3,14 +3,9 @@
 # Prof. Rogério Lazanha
 # 01/05/2022
 
-# 1- Faca um programa que determine o mostre os cinco primeiros multiplos de 3
-# considerando numeros maiores que 0.
+# 2- Escreva um programa que escreva na tela, de 1 ate 100, de 1 em 1.
 
-print("Exibindo os 5 primeiros números múltiplos de 3")
-num = 0
-mult = 1
-while( mult <= 5 ):
-    if(num%3 == 0) and (num != 0):
-        print(num)
-        mult += 1
-    num += 1
+n = 1
+while( n <= 100 ):
+    print(n)
+    n += 1
