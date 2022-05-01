fcff51c896964635fb2d50db00cf484176c9b9ae
Augusto Finotti Oliveira <augustofoliver@gmail.com>
2022-05-01 16:57:46 -0300

    adding 3.py

---
+++ Lista de exercicios 3/3.py
@@ -0,0 +1,13 @@
+# FATEC Ourinhos - Programação 1 - Seg Info
+# Augusto Finotti Oliveira
+# Prof. Rogério Lazanha
+# 01/05/2022
+
+# 3- Faca um algoritmo utilizando o comando while que mostra uma contagem regressiva na tela,
+# iniciando em 10 e terminando em 0. Mostrar uma mensagem “FIM!” apos a contagem.
+
+n = 10
+while( n >= 1 ):
+    print(n)
+    n -= 1
+print("FIM!")
