3a4b5f9792cd36eb31b423fcd49d15c00c41b18a
Augusto Finotti Oliveira <augustofoliver@gmail.com>
2022-05-01 16:57:54 -0300

    adding 4.py

---
+++ Lista de exercicios 3/4.py
@@ -0,0 +1,12 @@
+# FATEC Ourinhos - Programação 1 - Seg Info
+# Augusto Finotti Oliveira
+# Prof. Rogério Lazanha
+# 01/05/2022
+
+# 4- Escreva um programa que declare um inteiro, inicialize-o com 0, e incremente-o de 1000 em 1000,
+# imprimindo seu valor na tela, ate que seu valor seja 100000 (cem mil). 
+
+n = 0
+while( n <= 100000 ):
+    print(n)
+    n += 1000
