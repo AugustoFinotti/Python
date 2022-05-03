ebdc596ed5750e088c478eb9da21f2e8c0970747
Augusto Finotti Oliveira <augustofoliver@gmail.com>
2022-05-03 13:27:30 -0300

    adding 10.py

---
+++ Lista de exercicios 3/10.py
@@ -0,0 +1,13 @@
+# FATEC Ourinhos - Programação 1 - Seg Info
+# Augusto Finotti Oliveira
+# Prof. Rogério Lazanha
+# 02/05/2022
+
+# 10- Faca um programa que calcule e mostre a soma dos 50 primeiros numeros pares.
+
+p, soma, n = 0, 0, 0
+while(p <= 50):
+    soma += n
+    p += 1
+    n += 2
+print(soma)
