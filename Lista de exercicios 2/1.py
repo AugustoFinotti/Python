5d3cff97ca0ded1cd3afe49a837e586085aae789
Augusto Finotti Oliveira <augustofoliver@gmail.com>
2022-05-03 15:37:01 -0300

    adding 1.py

--- Lista de exercicios 2/1.py
+++ Lista de exercicios 2/1.py
@@ -1,5 +1,16 @@
-num1 = input("Digite um número: ")
-num2 = input("Digite outro número: ")
+# FATEC Ourinhos - Programação 1 - Seg Info
+# Augusto Finotti Oliveira
+# Prof. Rogério Lazanha
+# 03/05/2022
 
+# 29 - Faca um programa que receba dois numeros e mostre qual deles eh o maior. 
+
+# Input
+num1 = float(input("Digite um número: "))
+num2 = float(input("Digite outro número: "))
+
+# Processing and Output
 if(num1 > num2):
-    
+    print("O maior número é: %.2f" %(num1))
+else:
+    print("O maior número é: %.2f" %(num2))
