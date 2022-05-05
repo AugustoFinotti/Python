e0b903da39c40965cdde99f15e5d6a19a89f70b9
Augusto Finotti Oliveira <augustofoliver@gmail.com>
2022-05-05 13:42:05 -0300

    adding 33.py

--- Lista de exercicios 2/33.py
+++ Lista de exercicios 2/33.py
@@ -6,6 +6,7 @@
 # 33- Um produto vai sofrer aumento de acordo com a tabela abaixo.
 # Leia o preco antigo, calcule e escreva o preco novo, e escreva uma mensagem em funcao do preco novo
 # (de acordo com a segunda tabela).
+
 # PRECO ANTIGO           |      PERCENTUAL DE AUMENTO
 # ate R$ 50              |              5%
 # entre R$ 50 e R$ 100   |              10%
@@ -23,3 +24,24 @@ else:
 
 prec_novo = prec_ant * aument
 print("O preço novo será de: %.2f R$" %(prec_novo))
+print()
+
+#      PRECO NOVO       |    MENSAGEM
+#      ate R$ 80              Barato
+#
+# entre R$ 80 e R$ 120
+#     (inclusive)             Normal
+
+# entre R$ 120 e R$ 200
+#     (inclusive)             Caro
+#
+# acima de R$ 200             Muito caro
+
+if(prec_novo<80):
+    print("Barato")
+elif(prec_novo>=80) and (prec_novo<=120):
+    print("Normal")
+elif(prec_novo>120) and (prec_novo<=200):
+    print("Caro")
+else:
+    print("Muito caro")
